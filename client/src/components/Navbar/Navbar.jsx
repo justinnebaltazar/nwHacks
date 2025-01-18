e400e9ef7423ca6e75e6e1eaddbcc0ba59f19133
aleung30 <aleung30@student.ubc.ca>
2025-01-18 15:41:39 -0800

    Added Navbar styling

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <ul>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Get Help</a>
+            </li>
+            <li>
+                <a href="#">Donate</a>
+            </li>
+            <li>
+                <a href="#">About Us</a>
+            </li>
+            <li>
+                <a href="#">Contact Us</a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
