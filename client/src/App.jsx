ad29e8f4bca0c1570306336db782a5930c7ec7bc
Justinne Baltazar <jfb3@sfu.ca>
2025-01-18 15:38:32 -0800

    Add Router for linking pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import styles from "./App.module.css"
 import LoginPage from "./components/LoginPage/LoginPage"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
-    <div className={styles.App}>
-      <LoginPage></LoginPage>
-    </div>
+
+   <Router>
+    <Routes>
+      <Route path="/" element={<LoginPage/>}></Route>
+    </Routes>
+   </Router>
+
   )
 }
 
